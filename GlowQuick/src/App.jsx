22e4168640f62ba0cee0e99913ef7902286949f9
Hemant Medhsia <hemantmedhsia@gmail.com>
2024-11-23 17:23:25 +0530

    project setup done with the initial commit

--- GlowQuick/src/App.jsx
+++ GlowQuick/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-gray-900 flex justify-center items-center h-screen text-gray-100 text-3xl font-semibold'>Welcome to Glow Quick . . .</div>
+    <div className='bg-gray-900 flex justify-center items-center h-screen text-green-300 text-3xl font-semibold'>Welcome to Glow Quick . . .</div>
   )
 }
 

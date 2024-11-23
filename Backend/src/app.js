60b8c91e8b1307f5b59befedd07933e4a0c2303e
Hemant Medhsia <hemantmedhsia@gmail.com>
2024-11-23 17:20:34 +0530

    initial commit

---
+++ Backend/src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import {fileURLToPath} from 'url';
+import path from 'path';
+dotenv.config({
+    path: './.env',
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(cors( {
+    origin: process.env.CORS_ORIGIN,
+    credientials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+export default app;
---
+++ GlowQuick/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-900 flex justify-center items-center h-screen text-gray-100 text-3xl font-semibold'>Welcome to Glow Quick . . .</div>
+  )
+}
+
+export default App;

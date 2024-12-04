d908f5c9bb154b28b077cebe098017370de44330
Hemant Medhsia <hemantmedhsia@gmail.com>
2024-12-04 17:24:35 +0530

    creating models

---
+++ Backend/src/Models/Token.Model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const tokenSchema = mongoose.Schema( {
+    tokenNumber: {
+        type: Number,
+        required: true,
+    },
+    TokenDateAndTime: {
+        type: Date,
+        required: true,
+    },
+    serviceMan: {
+        ref: 'Barber',
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    customer: {
+        ref: 'Customer',
+        type: mongoose.Schema.Types.ObjectId,
+    },
+})
+
+const Token = mongoose.model('Token', tokenSchema);
+
+export default Token;

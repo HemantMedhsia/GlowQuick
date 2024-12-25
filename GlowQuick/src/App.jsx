172e74e5eca4f462dda55cc02d4e4711c3213529
Hemant Medhsia <hemantmedhsia@gmail.com>
2024-12-25 21:09:32 +0530

    initial commit

--- GlowQuick/src/App.jsx
+++ GlowQuick/src/App.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import BrowserRouter, Routes, and Route
 import Landingpage from "./Pages/Landingpage";
 import CursorFollower from "./Components/CursorFollower/CursorFollower";
-import Button1 from "./Common/Button1/Button1";
-import Button2 from "./Common/Button2/Button2";
-import Button3 from "./Common/Button3/Button3";
+import LoginPage from "./Pages/LoginPage";
 
 const App = () => {
   return (
-    <div className="bg-[#cfe5ed] relative text-green-300 text-3xl font-semibold">
-      <Landingpage />
-      <CursorFollower />
-      {/* <Button3/> */}
-    </div>
+    <Router>
+      <div className="bg-[#cfe5ed] relative text-green-300 text-3xl font-semibold">
+        <CursorFollower />
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
---
+++ GlowQuick/src/Pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='h-screen flex justify-center items-center bg-gray-900'>LoginPage</div>
+  )
+}
+
+export default LoginPage;
